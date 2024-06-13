043312555509fc36810969ec7928a8f895526cfb
Shimul Hossain <shimultechtips@gmail.com>
2024-06-13 10:44:48 +0600

    08-B-02

---
+++ routers/userRouter.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+const { signIn, signUp } = require("../controllers/userControllers");
+
+router.route("/signup").post(signUp);
+router.route("/signin").post(signIn);
+
+module.exports = router;
