f626ad198105d237a9d28c5319d5ef1eefe3060f
Shimul Hossain <shimultechtips@gmail.com>
2024-06-15 20:12:12 +0600

    08-C-09

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -5,6 +5,7 @@ const {
   getProductById,
   updateProductById,
   getPhoto,
+  filterProducts,
 } = require("../controllers/productController");
 const admin = require("../middlewares/admin");
 const authorize = require("../middlewares/authorize");
@@ -17,5 +18,7 @@ router
   .put([authorize, admin], updateProductById);
 
 router.route("/photo/:id").get(getPhoto);
+
+router.route("/filter").post(filterProducts);
 
 module.exports = router;
