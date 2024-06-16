51b1757dd6ad3ee36a1533ec72b558f36a99461e
Shimul Hossain <shimultechtips@gmail.com>
2024-06-16 21:53:42 +0600

    08-D-05

---
+++ controllers/profileController.js
@@ -0,0 +1,6 @@
+const _ = require("lodash");
+const { Profile } = require("../models/profile");
+
+module.exports.getProfile = async (req, res) => {};
+
+module.exports.setProfile = async (req, res) => {};
