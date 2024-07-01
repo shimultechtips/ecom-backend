8f327c99d3ae956f3bb4212470c0ab3319dbd779
Shimul Hossain <shimultechtips@gmail.com>
2024-07-01 12:14:39 +0600

    Vercel Deploy

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require("express-async-errors");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const error = require("./middlewares/error");
 
 require("./middlewares")(app);
 require("./middlewares/routes")(app);
 
+app.use(cors());
 app.use(error);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,11 +2,18 @@ require("dotenv/config");
 const mongoose = require("mongoose");
 const app = require("./app");
 
+const DB = process.env.MONGODB_SERVER.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD
+);
+
 mongoose
-  .connect(process.env.MONGODB_URL_LOCAL)
+  .connect(DB)
   .then(() => console.log("Connected To MongoDB!"))
-  .catch((err) => console.error("Connection Error!"));
+  .catch((err) => console.log("MongoDB Connection Failed!"));
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log("Listening On Port : ", port));
+app.listen(port, () => console.log(`Listening On Port ${port}`));
+
+module.exports = app;
