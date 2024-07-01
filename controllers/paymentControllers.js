ba4f4fb3d42e4c05fa481b6d44c0eefa1343fc26
Shimul Hossain <shimultechtips@gmail.com>
2024-07-01 17:40:13 +0600

    08-H-08

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -35,7 +35,7 @@ module.exports.initPayment = async (req, res) => {
     success: "yoursite.com/success", // If payment Succeed
     fail: "yoursite.com/fail", // If payment failed
     cancel: "yoursite.com/cancel", // If user cancel payment
-    ipn: "yoursite.com/ipn", // SSLCommerz will send http post request in this link
+    ipn: "https://ecom-backend-topaz.vercel.app/api/payment/ipn", // SSLCommerz will send http post request in this link
   });
 
   // Set order details
